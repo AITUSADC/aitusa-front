bed57872bd1fefc1115a6cd791b4cfc108e7c32a
arlan-6 <arlanhan1997@gmail.com>
2026-04-22 11:35:49 +0500

    feat: implement clubs page with mock data fetching example

--- app/clubs/page.tsx
+++ app/clubs/page.tsx
@@ -1,5 +1,33 @@
+"use client";
 import React from "react";
 // localhost:3000/clubs
+
+type Club = {
+  id: number;
+  name: string;
+  description: string;
+};
+
 export default function Clubs() {
-  return <div> Aitusa clubs page</div>;
+  const [clubs, setClubs] = React.useState<Club[]>([]);
+  React.useEffect(() => {
+    fetch("/api/clubs-mock-data")
+      .then((res) => res.json())
+      .then((data) => setClubs(data));
+  }, []);
+  return (
+    <div>
+      <h1>Clubs Page</h1>
+      <div className="">
+        {
+          clubs.map((club) => (
+            <div key={club.id} className="border p-4 mb-4">
+              <h2 className="text-xl font-bold">{club.name}</h2>
+              <p>{club.description}</p>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
 }
